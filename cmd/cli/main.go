ff7c3e8876d8a16b9b5f4203652d5a3ae557d064
奶爸 <hi@nai.ba>
2019-12-12 23:16:50 +0800

    交互式命令行

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,15 +1,84 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
 
+	"github.com/c-bata/go-prompt"
 	"github.com/p14yground/cook/model"
 )
 
-func main() {
-	conf, err := model.ReadInConfig("./config.yaml")
+const welcome = `________________ _______ ______ __
+__  ____/__  __ \__  __ \___  //_/
+_  /     _  / / /_  / / /__  ,<   
+/ /___   / /_/ / / /_/ / _  /| |  
+\____/   \____/  \____/  /_/ |_|
+
+Welcome to cook!`
+
+const (
+	_ = iota
+	cTypeSetTag
+)
+
+var servers map[string][]*model.Server
+var tags []string
+var configFilePath string
+
+func init() {
+	servers = make(map[string][]*model.Server)
+}
+
+func log(format string, args ...interface{}) {
+	fmt.Printf("🐮[LOG]"+format+"\n", args...)
+}
+
+func loadConfig() {
+	conf, err := model.ReadInConfig(configFilePath)
 	if err != nil {
 		panic(err)
 	}
-	log.Printf("conf:%v", conf)
+	for i := 0; i < len(conf.Servers); i++ {
+		server := conf.Servers[i]
+		for j := 0; j < len(conf.Servers[i].Tags); j++ {
+			tag := conf.Servers[i].Tags[j]
+			if len(servers[tag]) == 0 {
+				tags = append(tags, tag)
+			}
+			servers[tag] = append(servers[tag], &server)
+		}
+	}
+}
+
+var suggests = []prompt.Suggest{
+	{Text: "dial", Description: "连接到主机"},
+	{Text: "quit", Description: "退出 Cook"},
+}
+
+func completer(d prompt.Document) []prompt.Suggest {
+	return prompt.FilterHasPrefix(suggests, d.GetWordBeforeCursor(), true)
+}
+
+func execCommand(command string) {
+	args := strings.Split(command, " ")
+	switch args[0] {
+	case "quit":
+		os.Exit(0)
+	}
+	log("args %v", args)
+}
+
+func main() {
+	flag.StringVar(&configFilePath, "conf", "./config.yaml", "要加载的配置文件")
+	flag.Parse()
+	loadConfig()
+	fmt.Println(welcome)
+	log("load %d servers, %d tags.", len(servers), len(tags))
+	log("configFile: %s", configFilePath)
+	for {
+		t := prompt.Input("", completer, prompt.OptionPrefix("🐮[CMD]> "))
+		execCommand(t)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module github.com/p14yground/cook
 go 1.13
 
 require (
+	github.com/c-bata/go-prompt v0.2.3
 	github.com/fsnotify/fsnotify v1.4.7
+	github.com/mattn/go-runewidth v0.0.7 // indirect
+	github.com/mattn/go-tty v0.0.3 // indirect
+	github.com/pkg/term v0.0.0-20190109203006-aa71e9d9e942 // indirect
 	github.com/spf13/viper v1.6.1
-	gopkg.in/fsnotify.v1 v1.4.7
+	github.com/stretchr/testify v1.4.0 // indirect
 )
